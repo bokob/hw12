75c201d7affedb4cc7cb95b2a391ccd4469a1db9
bokob <bmc0520@naver.com>
2022-06-02 19:45:08 +0900

    모든 주석 작성 완료

--- hw12-sorting-hashing.c
+++ hw12-sorting-hashing.c
@@ -127,6 +127,7 @@ int freeArray(int *a)	// 배열 해제
 {
 	if(a != NULL)	// a가 NULL이 아닌 경우. 배열이 존재하는 경우
 		free(a);	// a 해제
+		
 	return 0;
 }
 
@@ -177,6 +178,7 @@ int selectionSort(int *a) 	// 선택정렬
 
 	printf("----------------------------------------------------------------\n");
 	printArray(a);	// 정렬 후 배열 출력
+
 	return 0;
 }
 
@@ -248,21 +250,24 @@ int shellSort(int *a)	// 셸정렬
 	printArray(a);	// 정렬 전 배열 출력
 
 	for (h = MAX_ARRAY_SIZE/2; h > 0; h /= 2) // h는 부분집합의 기준이 되는 간격
-	// h에 MAX_ARRAY_SIZE / 2 의 값을 넣는다. h는 1/2씩 줄어든다. 이 일을 h가 0보다 큰 경우에만 반복 
+	/* h에 MAX_ARRAY_SIZE / 2 의 값을 넣는다. h는 1/2씩 줄어든다. 
+	이 일을 h가 0보다 큰 경우에만 반복 */
 	{
 		for (i = 0; i < h; i++)	// h의 크기 만큼 반복
 		{
 			for(j = i + h; j < MAX_ARRAY_SIZE; j += h)
-			// 
+			/* j에 i+h의 값을 넣는다. 한 번 루프를 돌 때 j에 j+h의 값을 넣는다.
+			이 일을 j가 MAX_ARRAY_SIZE 크기보다 작은 경우에만 반복 */
 			{
-				v = a[j];
-				k = j;
+				v = a[j];	// v에 a[j]의 값을 넣는다.
+				k = j;		// k에 j의 값을 넣는다.
 				while (k > h-1 && a[k-h] > v)
+				// k가 h-1 보다 크고 a[k-h]가 v보다 큰 경우에 반복
 				{
-					a[k] = a[k-h];
-					k -= h;
+					a[k] = a[k-h];	// a[k]에 a[k-h]의 값을 넣는다.
+					k -= h;	// k에 k-h의 값을 넣는다.
 				}
-				a[k] = v;
+				a[k] = v;	// a[k]에 v를 넣는다.
 			}
 		}
 	}
@@ -274,33 +279,44 @@ int shellSort(int *a)	// 셸정렬
 
 int quickSort(int *a, int n)	// 퀵정렬
 {
-	int v, t;
+	int v, t;	
 	int i, j;
 
-	if (n > 1)
+	if (n > 1)	// n이 1보다 큰 경우
 	{
-		v = a[n-1];
-		i = -1;
-		j = n - 1;
+		v = a[n-1];	// v에 a[n-1]의 값을 넣는다. v는 퀵정렬에서의 pivot이다.
+		i = -1;		// i를 -1로 초기화
+		j = n - 1;	// j에 n-1을 넣는다.
 
 		while(1)
 		{
-			while(a[++i] < v);
-			while(a[--j] > v);
+			while(a[++i] < v);	
+			/* i에 1을 더한 후, a[i]가 v보다 작은 경우에 반복 
+			pivot보다 큰 값 찾는 과정 */
+			while(a[--j] > v);	
+			/* j에 1을 뺀 후, a[j]가 v보다 큰 경우에 반복 
+			pivot보다 작은 값 찾는 과정 */
 
-			if (i >= j) break;
-			t = a[i];
+			if (i >= j) break;	// i가 j 이상인 경우 탈출
+
+			/* 위에서 찾은 pivot의 왼쪽에서 pivot보다 큰 수와 
+			pivot의 오른쪽에서 pivot보다 작은 수끼리 자리를 바꾼다. */
+			t = a[i];		
 			a[i] = a[j];
 			a[j] = t;
 		}
+
+		/* pivot보다 큰 값의 인덱스가 pivot보다 작은 값의 인덱스보다 큰 경우.
+		i, j 인덱스 탐색이 엇갈린 경우에 실행되는 부분.
+		pivot보다 큰 값과 작은 값끼리 자리를 바꾼다. */
+
 		t = a[i];
 		a[i] = a[n-1];
 		a[n-1] = t;
 
-		quickSort(a, i);
-		quickSort(a+i+1, n-i-1);
+		quickSort(a, i); // pivot 값을 중심으로 왼쪽 부분을 재귀적을 정렬
+		quickSort(a+i+1, n-i-1); // pivot 값을 중심으로 오른쪽 부분을 재귀적을 정렬
 	}
-
 
 	return 0;
 }
@@ -312,45 +328,56 @@ int hashCode(int key) // 해시 코드 생성
 
 int hashing(int *a, int **ht)	// 배열 a에 대한 hash table 생성
 {
-	int *hashtable = NULL;
+	int *hashtable = NULL;	// int 형에 대한 포인터 hashtable에 NULL을 넣는다.
 
-	/* hash table이 NULL인 경우 메모리 할당 */
-	if(*ht == NULL) 
+	if(*ht == NULL)	// ht가 NULL인 경우
 	{
 		hashtable = (int*)malloc(sizeof(int) * MAX_ARRAY_SIZE);
+		/* 메모리의 Heap 영역에 4바이트 * MAX_ARRAY_SIZE 만큼의 크기를 만들고,
+		그곳의 주소를 hashtable에 넣는다.
+
 		*ht = hashtable;  
 		/* 할당된 메모리의 주소를 복사 --> main에서 배열을 control 할수 있도록 함*/
 	} 
-	else 
+	else	// ht가 NULL이 아닌 경우 
 	{
-		hashtable = *ht;	/* hash table이 NULL이 아닌경우, table 재활용, reset to -1 */
+		hashtable = *ht;	
+		// hashtable에 ht가 가리키는 곳에 들어있느 주소를 넣는다.
+		// 원래 있던 해시테이블을 재활용하는 것이다.
 	}
 
-	for(int i = 0; i < MAX_HASH_TABLE_SIZE; i++)
-		hashtable[i] = -1;
+	for(int i = 0; i < MAX_HASH_TABLE_SIZE; i++)	// MAX_HASH_TABLE_SIZE 크기 만큼 반복
+		hashtable[i] = -1;	// hashtable[i]를 -1로 초기화
 
-	int key = -1;
-	int hashcode = -1;
-	int index = -1;
-	for (int i = 0; i < MAX_ARRAY_SIZE; i++)
+	int key = -1;		// key -1로 초기화
+	int hashcode = -1;	// hashcode -1로 초기화
+	int index = -1;		// index -1로 초기화
+	for (int i = 0; i < MAX_ARRAY_SIZE; i++)	// MAX_ARRAY_SIZE 크기 만큼 반복
 	{
-		key = a[i];
-		hashcode = hashCode(key);
+		key = a[i];					// key에 a[i]의 값을 넣는다.
+		hashcode = hashCode(key);	
+		// hashcode에 hashCode(key)의 결과로 나온 해시코드를 넣는다.
 	
 		if (hashtable[hashcode] == -1)
+		// hashtable[hashcode]가 -1인 경우. 다른 key가 들어있지 않은 상태
 		{
-			hashtable[hashcode] = key;
+			hashtable[hashcode] = key;	// hashtable[hashcode]에 key를 넣는다.
 		} 
-		else 	
+		else // hashtable[hashcode]가 -1이 아닌 경우. 다른 key가 들어있는 상태
+		// 충돌 발생
 		{
-
-			index = hashcode;
+			index = hashcode;	// index에 hashcode의 값을 넣는다.
 
 			while(hashtable[index] != -1)
+			// hashtable[index] 가 -1이 아닌 경우 반복
 			{
 				index = (++index) % MAX_HASH_TABLE_SIZE
+				/* index에 index에 1을 더하고 그 결과에 
+				MAX_HASH_TABLE_SIZE를 나눴을 때의 나머지를 넣는다.*/
 			}
-			hashtable[index] = key;
+			
+			// key를 넣을 수 있는 공간을 찾은 상태
+			hashtable[index] = key; // hashtable[index]에 key의 값을 넣는다.
 		}
 	}
 
@@ -370,5 +397,6 @@ int search(int *ht, int key)	// 해시 테이블에서 key를 찾아 해시 테이블의 인덱스 
 		index = index % MAX_HASH_TABLE_SIZE;
 		// index에 index를 MAX_HASH_TABLE_SIZE로 나눴을 때의 나머지를 넣는다.
 	}
+
 	return index;	//  인덱스 반환
 }
